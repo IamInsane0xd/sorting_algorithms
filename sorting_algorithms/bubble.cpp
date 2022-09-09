59b55f4de765650c6efd17a88694fda34796b721
IamInsane0xd <biczo.ferenc001@gmail.com>
2022-09-09 16:50:11 +0200

    more funny naming

--- sorting_algorithms/bubble.cpp
+++ sorting_algorithms/bubble.cpp
@@ -3,22 +3,22 @@
 void bubbleSort(std::vector<int> &arr)
 {
 	int n = arr.size(), i, j, t;
-	bool swapped;
+	bool s;
 
 	for (i = 0; i < n - 1; i++)
 	{
-		swapped = false;
+		s = false;
 
 		for (j = 0; j < n - i - 1; j++)
 		{
 			if (arr.at(j) > arr.at(j + 1))
 			{
 				swap(arr, j, j + 1);
-				swapped = true;
+				s = true;
 			}
 		}
 
-		if (!swapped)
+		if (!s)
 			break;
 	}
 }
